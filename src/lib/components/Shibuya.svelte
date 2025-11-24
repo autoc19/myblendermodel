0846dcef56e1a6f341cbda91ae05f04cd0fcc579
auto <auto@autos-Mac-mini.local>
2025-11-24 10:36:34 +0800

    Add Shibuya model to scene

---
+++ src/lib/components/Shibuya.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { T, useGLTF } from '@threlte/core'
+
+  const gltf = useGLTF('/models/shibuya.glb')
+</script>
+
+{#await gltf}
+  <p>Loading...</p>
+{:then model}
+  <T.Primitive object={model.scene} />
+{/await}
